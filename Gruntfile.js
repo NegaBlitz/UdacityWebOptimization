5b92bfd511de6e1d304cdd8974f44604cd973874
Colin <silvercpb@gmail.com>
2017-07-28 09:51:59 -0400

    Server for checking PSI is now installed via node

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,11 +31,25 @@ module.exports = function(grunt) {
         }]
       }
     },
+		
+		//Connect
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: 'www-root'
+				}
+			}
+		},
 	});
 	
 	//Load plugins
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-responsive-images');
+	// grunt.loadNpmTasks('grunt-http-server');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	
 	//Register tasks to run
+  grunt.registerTask('default', ['responsive_images']);
+	
 };
